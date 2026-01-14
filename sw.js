98f3a878306ba5a45f41c202043e723c19649a73
webgame2026 <mrdarpon@gmail.com>
2026-01-15 01:47:11 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,27 @@
 const CACHE_NAME = 'mindwhack-v1';
+const BASE_PATH = '/mindwhack/';
+
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+  BASE_PATH,
+  BASE_PATH + 'index.html',
+  BASE_PATH + 'manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
   );
 });
